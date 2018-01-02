adbaeb4ccaa01b8ad13562f7aaa6b38df5958283
Michael Lange <dingoeatingfuzz@gmail.com>
2018-01-01 19:50:40 -0800

    Make the home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
+import './index.scss';
+
 const IndexPage = () => (
-  <div>
-    <h1>Hi dudes</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2">Go to page 2</Link>
+  <div className="layout">
+    <section>
+      <div className="label">
+        <h1>Michael Lange</h1>
+        <p>Maker of things</p>
+      </div>
+    </section>
+    <nav>
+      <ol>
+        <li><Link to="/writing">Projects</Link></li>
+        <li><Link to="/writing">Writing</Link></li>
+        <li><Link to="/writing">Long Form</Link></li>
+        <li><Link to="/writing">Talks</Link></li>
+        <li><Link to="/writing">Archive</Link></li>
+      </ol>
+    </nav>
   </div>
 )
 
