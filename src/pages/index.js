b6148e5459e78a373a60f4899cc421f1d08b7776
Michael Lange <dingoeatingfuzz@gmail.com>
2019-01-23 01:01:47 -0800

    Add visualization container to the home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import CommonNavigation from '../components/common-navigation'
 
 import Layout from '../components/layout'
 import './index.scss'
+import VisualizationContainer from '../components/visualization-container';
 
 const IndexPage = () => (
   <Layout>
     <div className="home-page">
       <section>
+        <VisualizationContainer />
         <div className="label">
           <h1>Michael Lange</h1>
           <p>Maker of things</p>
