761fd761890496ee9f7760d69d1ae5c86d0c2648
Michael Lange <dingoeatingfuzz@gmail.com>
2019-02-18 22:30:32 -0800

    Talks page finished

--- src/pages/talks.js
+++ src/pages/talks.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-import CommonHeader from '../components/common-header'
+import { graphql } from 'gatsby'
+import Link from 'gatsby-link'
 
 import Layout from '../components/layout'
-import CommonFooter from '../components/common-footer';
+import CommonHeader from '../components/common-header'
+import CommonFooter from '../components/common-footer'
 import './talks.scss'
 
-const TalksPage = () => (
+const talkSort = (a, b) => new Date(b.node.metadata.date) - new Date(a.node.metadata.date)
+const formatDate = d => `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}`
+
+const TalksPage = ({ data: { allSlidesMarkdown: data } }) => (
   <Layout>
     <div className="talks-page">
       <Helmet
@@ -14,11 +19,43 @@ const TalksPage = () => (
         description="Video, transcripts, and written transpositions of talks I've given."
       />
       <CommonHeader />
-
-      <section>Coming Soon.</section>
+      <main>
+        <ol className="talk-list">
+          {data.edges.sort(talkSort).map(({ node: talk }) => (
+            <li key={talk.id}>
+              <h2>
+                <Link to={talk.slug}>
+                  {talk.metadata.title}
+                </Link>
+              </h2>
+              <h3>{formatDate(new Date(talk.metadata.date))}</h3>
+              <div dangerouslySetInnerHTML={{ __html: talk.abstract}} />
+            </li>
+          ))}
+        </ol>
+      </main>
       <CommonFooter />
     </div>
   </Layout>
 )
 
 export default TalksPage
+
+export const query = graphql`
+  query Talks {
+    allSlidesMarkdown {
+      edges {
+        node {
+          id
+          slug
+          abstract
+          metadata {
+            title
+            conferences
+            date
+          }
+        }
+      }
+    }
+  }
+`
