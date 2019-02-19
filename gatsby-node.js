e7e2c60ee7df279ffe375f6ff8ad6cbc15bfbd4a
Michael Lange <dingoeatingfuzz@gmail.com>
2019-02-18 22:28:48 -0800

    New transformer for custom "slides markdown"

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,17 @@
 const path = require('path')
 const { createFilePath } = require('gatsby-source-filesystem')
+const normalizeNewline = require('normalize-newline')
+const jsYaml = require('js-yaml')
+const remark = require('remark')
+const remarkRecommended = require('remark-preset-lint-recommended')
+const remarkHtml = require('remark-html')
+const remarkReport = require('vfile-reporter')
 
-exports.onCreateNode = ({ node, getNode, actions }) => {
+const SLIDEREG = /\n?---\n/
+
+exports.onCreateNode = async ({ node, getNode, actions, loadNodeContent, createContentDigest, createNodeId }) => {
+  const { createNode, createParentChildLink, createNodeField } = actions
+
   if (node.internal.type === 'MarkdownRemark') {
     const fileNode = getNode(node.parent)
     const [,sourceDirectory] = node.fileAbsolutePath.split('/').reverse();
@@ -21,33 +31,88 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
         break;
     }
 
-    actions.createNodeField({
+    createNodeField({
       node,
       name: 'slug',
       value: slug
     })
 
-    actions.createNodeField({
+    createNodeField({
       node,
       name: 'type',
       value: type
     })
 
-    actions.createNodeField({
+    createNodeField({
       node,
       name: 'id',
       value: id.replace(/^.(.+).$/, '$1')
     })
   }
+
+  // Slide Markdown files
+  if (node.extension === 'smd') {
+    const raw = await loadNodeContent(node)
+    const processor = remark().use(remarkRecommended).use(remarkHtml)
+
+    let [metadata, abstract, ...rawSlides] = normalizeNewline(raw).split(SLIDEREG).slice(1)
+    metadata = jsYaml.load(metadata)
+
+    const generate = async item => {
+      return new Promise((resolve, reject) => {
+        processor.process(item, async (err, file) => {
+          if (err) {
+            const message = remarkReport(err)
+            console.error(message)
+            reject(message)
+          }
+
+          resolve({
+            markdown: item,
+            html: String(file)
+          })
+        })
+      });
+    }
+
+    const slides = await Promise.all(rawSlides.map(generate))
+
+    console.log('\n\n????\n');
+    console.log(slides);
+    processor.process(abstract, (err, file) => {
+      err && console.error(remarkReport(err))
+      abstract = String(file)
+    })
+
+    const data = {
+      metadata,
+      abstract,
+      slides,
+      slug: '/talks' + createFilePath({ node, getNode, basePath: 'talks' }),
+      filename: node.name
+    }
+    const smdNode = {
+      ...data,
+      id: metadata.id ? metadata.id : createNodeId(`${node.id} >>> SMD`),
+      children: [],
+      parent: node.id,
+      internal: {
+        contentDigest: createContentDigest(data),
+        type: 'SlidesMarkdown'
+      }
+    }
+    createNode(smdNode)
+    createParentChildLink({ parent: node, child: smdNode })
+  }
 }
 
-exports.createPages = ({ graphql, actions }) => {
+exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
   const typeToTemplate = {
     project: './src/templates/project-detail.js',
     blog: './src/templates/blog-post.js',
   }
-  return new Promise((resolve, reject) => {
+  await new Promise((resolve, reject) => {
     graphql(`
       {
         allMarkdownRemark {
@@ -71,8 +136,35 @@ exports.createPages = ({ graphql, actions }) => {
             id: node.fields.id,
           },
         })
-        resolve()
       })
+      resolve()
+    })
+  })
+  await new Promise((resolve, reject) => {
+    graphql(`
+      {
+        allSlidesMarkdown {
+          edges {
+            node {
+              slug
+              filename
+            }
+          }
+        }
+      }
+    `).then(result => {
+      result.data.allSlidesMarkdown.edges.forEach(({ node }) => {
+        console.log('hrmm', node.filename, `/static\\/slides\\/${node.filename}$/`)
+        createPage({
+          path: node.slug,
+          component: path.resolve('./src/templates/talk.js'),
+          context: {
+            slug: node.slug,
+            slides: `/static\/slides\/${node.filename}$/`
+          }
+        })
+      })
+      resolve()
     })
   })
 }
