5e25a3d976fa31c9c0bd39656df97dd2b8053f13
Michael Lange <dingoeatingfuzz@gmail.com>
2019-04-20 12:22:15 -0700

    Add host to thumbnail, despite not working locally

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Helmet from 'react-helmet'
+import { graphql } from 'gatsby'
 import CommonNavigation from '../components/common-navigation'
 
 import Layout from '../components/layout'
@@ -8,7 +9,7 @@ import VisualizationContainer from '../components/visualization-container'
 import './index.scss'
 import thumbnail from '../posts/thumbnails/default.png'
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <Helmet>
       <title>Maker of Things</title>
@@ -16,12 +17,12 @@ const IndexPage = () => (
       <meta name="twitter:site" content="Michael Lange" />
       <meta name="twitter:title" content="Michael Lange" />
       <meta name="twitter:description" content="Maker of Things" />
-      <meta name="twitter:image" content={thumbnail} />
+      <meta name="twitter:image" content={'https://' + data.site.siteMetadata.host + thumbnail} />
 
       <meta property="og:title" content="Michael Lange" />
       <meta property="og:site_name" content="Michael Lange" />
       <meta property="og:locale" content="en_US" />
-      <meta property="og:image" content={thumbnail} />
+      <meta property="og:image" content={'https://' + data.site.siteMetadata.host + thumbnail} />
     </Helmet>
     <div className="home-page">
       <section>
@@ -44,3 +45,13 @@ const IndexPage = () => (
 )
 
 export default IndexPage
+
+export const query = graphql`
+  query IndexQuery {
+    site {
+      siteMetadata {
+        host
+      }
+    }
+  }
+`;
