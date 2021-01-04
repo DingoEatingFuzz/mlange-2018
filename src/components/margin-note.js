907ce2bcb29f9f35e2de650e0789813439216891
Michael Lange <dingoeatingfuzz@gmail.com>
2021-01-03 18:22:54 -0800

    Ditch the lines, they are buggy and I no longer care

--- src/components/margin-note.js
+++ src/components/margin-note.js
@@ -18,33 +18,6 @@ const Note = ({ anchor, column = 0, y = 0, children }) => {
       throw new Error("Margin notes requires a pre-existing #margins element");
 
     const x = 900 + column * 220;
-    const anchorEl = document.getElementById(anchor);
-
-    let line;
-    if (anchorEl) {
-      const bbox = anchorEl.getBoundingClientRect();
-      const ax = bbox.x + bbox.width / 2;
-      const ay = bbox.y + bbox.height / 2;
-
-      const isAbove = ay - y > 0;
-      const dx = Math.abs(ax - x);
-      const dy = Math.abs(ay - y);
-
-      line = (
-        <svg
-          transform={`translate(${ax - x}, ${isAbove ? 0 : ay - y})`}
-          width={dx}
-          height={dy + 50}
-        >
-          <line
-            x1={0}
-            y1={isAbove ? dy : 0}
-            x2={dx + 5}
-            y2={isAbove ? 50 : dy + 50}
-          />
-        </svg>
-      );
-    }
 
     const note = (
       <div
@@ -52,7 +25,6 @@ const Note = ({ anchor, column = 0, y = 0, children }) => {
         style={{ left: x - 900, top: y }}
         data-anchor-id={anchor}
       >
-        {line ? line : null}
         <div className="margin-note-note">{children}</div>
       </div>
     );
